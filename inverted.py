760a11a3d1b644de1bf16243df1619d99df95d47
GLong Long <mgl2150@GLongs-MacBook-Pro.local>
2024-01-09 04:05:06 +0700

    tach+chimucnguoc

--- inverted.py
+++ inverted.py
@@ -1,30 +1,36 @@
 import json
+from collections import defaultdict
+import string
 
 def create_inverse_index(json_data):
-    inverse_index = {}
+    inverse_index = defaultdict(list)
     for key, value in json_data.items():
-        if isinstance(value, str):  
-            words = value.split()
-            for word in words:
-                if word not in inverse_index:
-                    inverse_index[word] = [key]
-                else:
-                    inverse_index[word].append(key)
-        elif isinstance(value, list):  
-            for item in value:
-                words = item.split() if isinstance(item, str) else str(item).split()
-                for word in words:
-                    if word not in inverse_index:
-                        inverse_index[word] = [key]
-                    else:
-                        inverse_index[word].append(key)
+        if isinstance(value, str):
+            translator = str.maketrans('', '', string.punctuation)
+            cleaned_text = value.translate(translator).lower()
+
+            words = cleaned_text.split()
+
+            for idx, word in enumerate(words):
+                inverse_index[word].append((key, idx))
+
+        elif isinstance(value, list):  # Check if the value is a list
+            for idx, item in enumerate(value):
+                if isinstance(item, str):
+                    translator = str.maketrans('', '', string.punctuation)
+                    cleaned_text = item.translate(translator).lower()
+
+                    words = cleaned_text.split()
+
+                    for word_idx, word in enumerate(words):
+                        inverse_index[word].append((key, idx, word_idx))
+
     return inverse_index
 
-with open('test.json', 'r') as file:
+with open('./test.json', 'r') as file:
     json_data = json.load(file)
 
 inverse_index = create_inverse_index(json_data)
 
 for word, indices in inverse_index.items():
     print(f'{word}: {indices}')
-
